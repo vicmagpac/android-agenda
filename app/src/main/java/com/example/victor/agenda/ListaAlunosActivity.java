77f5c4201ea115f587e15684908496be1948394b
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2016-12-02 16:22:57 -0300

    Enviando projeto inicial

---
+++ app/src/main/java/com/example/victor/agenda/ListaAlunosActivity.java
@@ -0,0 +1,20 @@
+package com.example.victor.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Victor", "Rafael", "Hellen"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+}
